c98d9546a23e3c3c58ff0c46f28edb34d62468ff
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-10 16:52:52 -0700

    Sync read file and process to stdout stream

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -4,7 +4,11 @@
 
 "use strict";
 
-// Minimist is a built-in function that applies conventions to command line inputs. The second parameter is default configs.
+// path is built into node
+const path = require("path");
+const fs = require("fs");
+
+// Minimist is a function/library that applies conventions to command line inputs. The second parameter is default configs.
 // We use slice(2) here because the first argument is always the path the node, and the second is always the path to the file. After that is an array of all the input values.
 const args = require("minimist")(process.argv.slice(2), {
   boolean: ["help"],
@@ -14,13 +18,19 @@ const args = require("minimist")(process.argv.slice(2), {
 if (args.help) {
   printHelp();
 } else if (args.file) {
-  console.log(args.file);
+  processFile(path.resolve(args.file));
 } else {
   error("Incorrect usage.", true);
 }
 
-
 // ********************
+
+function processFile(filepath) {
+  // If we don't specify an encoding as the second param we can't use console.log because readFileSync returns a Buffer, and console.log will print the Buffer as a string: <Buffer 48 65 6c 6c 6f 20 57 6f 72 6c 64 0a>
+  const contents = fs.readFileSync(filepath, "utf8");
+
+  process.stdout.write(contents);
+}
 
 function error(msg, includeHelp = false) {
   console.error(msg);
