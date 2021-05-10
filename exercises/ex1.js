1aaec2d8c30e85dd5585cdaed899049cf18c11d1
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-10 14:07:31 -0700

    Conditionally print based on CL args

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -10,17 +10,32 @@ const args = require("minimist")(process.argv.slice(2), {
   boolean: ["help"],
   string: ["file"],
 });
-console.log(args);
 
-// printHelp();
+if (args.help) {
+  printHelp();
+} else if (args.file) {
+  console.log(args.file);
+} else {
+  error("Incorrect usage.", true);
+}
+
 
 // ********************
 
+function error(msg, includeHelp = false) {
+  console.error(msg);
+  if (includeHelp) {
+    console.log("");
+    printHelp();
+  }
+}
+
 function printHelp() {
   console.log("ex1 usage:");
-  console.log("  ex1.js --help");
+  console.log("  ex1.js --file={FILENAME}");
   console.log("");
-  console.log("--help               print this help");
+  console.log("--help                 print this help");
+  console.log("--file={FILENAME}      process the file");
   console.log("");
 }
 // process.stdout.write("Hello world\n");
