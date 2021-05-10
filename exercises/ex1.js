efd781b60cef8cf0b4cfc10913023860a3105874
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-10 13:45:47 -0700

    Make file auto execute with node

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -1,1 +1,18 @@
-// My first Node.js program
+#! /usr/bin/env node
+
+// The above line means that env will find node on our system and use it to execute this file. This means we can execute it with "./ex1.js" rather than "./ex1.js node".
+
+"use strict";
+
+printHelp();
+
+// ********************
+
+function printHelp() {
+  console.log("ex1 usage:");
+  console.log("  ex1.js --help");
+  console.log("");
+  console.log("--help               print this help");
+  console.log("");
+}
+// process.stdout.write("Hello world\n");
